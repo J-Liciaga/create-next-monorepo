49e1b2ce97ff349f2ab675c786053eb702f01ab3
Josiah Liciaga Silva <josiah.liciagasilva@protonmail.com>
2023-02-15 15:36:52 -0500

    fix(project): rewrote indexjs script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# CREATE NX NEXT MONOREPO
+# CREATE-NEXT-MONOREPO
+
+Create a new project using the [nx-next-starter](https://github.com/J-Liciaga/nx-next-starter) template.
+
+## Usage
+
+Run the following command
+
+```
+npx create-next-monorepo
+```
--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 #!/usr/bin/env node
 
-const { spawn } = require("child_process");
+const { spawn, exec } = require("child_process");
+const fs = require("fs");
 
-const binary = spawn("./target/release/create-next-monorepo", process.argv.slice(2));
+function buildTarget() {
+    exec("yarn build", (err, stdout, stderr) => {
+        if (err) {
+            console.log(`Error running script: ${err}`);
+            return;
+        };
 
-binary.stdout.on("data", (data) => {
-    console.log(`stdout: ${data}`);
-});
+        console.log(`${stdout}`);
+        console.error(`${stderr}`);
+    });
+};
 
-binary.stderr.on("data", (data) => {
-    console.error(`stderr: ${data}`);
-});
+buildTarget();
 
-binary.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
-});
+const pathExists = (path) => {
+    try {
+        fs.accessSync(path);
+        return true;
+    } catch(err) {
+        return false;
+    };
+};
+
+const waitForBinary = setInterval(() => {
+    if (pathExists("./target")) {
+        setTimeout(() => {
+            buildBinary();
+        }, 1000);
+        execute();
+    };
+}, 100);
+
+const buildBinary = () => {
+    const binary = spawn("./target/release/create-next-monorepo", process.argv.slice(2));
+            
+    binary.stdout.on("data", (data) => {
+        console.log(`${data}`);
+    });
+    
+    binary.stderr.on("data", (data) => {
+        console.error(`Error: ${data}`);
+    });
+    
+    binary.on("close", (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+};
+
+const execute = () => clearInterval(waitForBinary);
