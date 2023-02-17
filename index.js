106cf43ca3383b3c6afb136141df6a6c0d3e3956
Josiah Liciaga Silva <josiah.liciagasilva@protonmail.com>
2023-02-17 18:08:33 -0500

    fix(project): rebuilding solution

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
 #!/usr/bin/env node
 
-const { spawn, exec } = require("child_process");
-const fs = require("fs");
+const { spawn } = require("child_process");
 
-function buildTarget() {
-    exec("yarn build", (err, stdout, stderr) => {
-        if (err) {
-            console.log(`Error running script: ${err}`);
-            return;
-        };
-
-        console.log(`${stdout}`);
-        console.error(`${stderr}`);
-    });
-};
-
-buildTarget();
-
-const pathExists = (path) => {
-    try {
-        fs.accessSync(path);
-        return true;
-    } catch(err) {
-        return false;
-    };
-};
-
-const waitForBinary = setInterval(() => {
-    if (pathExists("./target")) {
-        setTimeout(() => {
-            buildBinary();
-        }, 1000);
-        execute();
-    };
-}, 100);
-
-const buildBinary = () => {
+(function() {
     const binary = spawn("./target/release/create-next-monorepo", process.argv.slice(2));
             
     binary.stdout.on("data", (data) => {
@@ -48,7 +15,5 @@ const buildBinary = () => {
     
     binary.on("close", (code) => {
         console.log(`child process exited with code ${code}`);
-    });
-};
-
-const execute = () => clearInterval(waitForBinary);
+    }); 
+})(); 
